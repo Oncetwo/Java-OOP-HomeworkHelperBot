4b5d08406d7520c361d1b860ab9b056d65ef7796
OnceTwo <vnesterova662@gmail.com>
2025-11-04 10:06:00 +0500

    Try add in main best commit

---
+++ src/main/java/bot/schedule/ScheduleStorageException.java
@@ -0,0 +1,11 @@
+package bot.schedule;
+// чтобы бизнес-логика не знала о конкретных исключениях, а знала только то, что исключеение может возникнуть
+public class ScheduleStorageException extends RuntimeException {
+    public ScheduleStorageException(String message) { // Создает исключение с текстовым сообщением
+        super(message); // Передает сообщение в конструктор RuntimeException
+    }
+    
+    public ScheduleStorageException(String message, Throwable cause) { // Создает исключение с сообщением И причиной
+        super(message, cause);
+    }
+}
--- src/main/java/bot/user/SQLiteUserStorage.java
+++ src/main/java/bot/user/SQLiteUserStorage.java
@@ -1,4 +1,5 @@
 package bot.user;
+import bot.user.exception.UserStorageException;
 
 import java.sql.*;
 
@@ -25,7 +26,7 @@ public class SQLiteUserStorage implements UserStorageInterface {
             statment.close(); // закрываем statment (запрос на создание отправили, он больше не нужен)
             
         } catch (SQLException e) {
-            throw new RuntimeException("Ошибка инициализации базы данных", e); 
+            throw new UserStorageException("Ошибка инициализации базы данных", e); 
             // непроверяемое исключение (чтобы остановить выполнение программы с сообщением
         }
     }
@@ -62,7 +63,7 @@ public class SQLiteUserStorage implements UserStorageInterface {
             return null;
             
         } catch (SQLException e) {
-            throw new RuntimeException("Ощибка получения пользователя по id", e);
+            throw new UserStorageException("Ощибка получения пользователя по id", e);
         }
     }
 
@@ -70,7 +71,7 @@ public class SQLiteUserStorage implements UserStorageInterface {
     @Override
     public void saveUser(User user) { // сохранить нового пользователя
         if (userExists(user.getChatId())) { // проверка, существует ли пользователь уже
-            throw new RuntimeException("Пользователь уже существует");
+            throw new UserStorageException("Пользователь уже существует");
         }
         try {
             String sql = "INSERT INTO users (chatId, name, groupName, state, waitingForButton) VALUES (?, ?, ?, ?, ?)";
@@ -87,7 +88,7 @@ public class SQLiteUserStorage implements UserStorageInterface {
             pstatment.close();
             
         } catch (SQLException e) {
-            throw new RuntimeException("Ошибка сохранения пользователя", e);
+            throw new UserStorageException("Ошибка сохранения пользователя", e);
         }
     }
 
@@ -95,7 +96,7 @@ public class SQLiteUserStorage implements UserStorageInterface {
     @Override
     public void updateUser(User user) { // обновить пользователя
         if (!userExists(user.getChatId())) { // проверка на существование пользоваетля
-            throw new RuntimeException("Пользоваетля с таким ID еще не существует в базе данных");
+            throw new UserStorageException("Пользоваетля с таким ID еще не существует в базе данных");
         }
         try {
             String sql = "UPDATE users SET name = ?, groupName = ?, state = ?, waitingForButton = ? WHERE chatId = ?";  // обновляет поля пользователя с указанным ID
@@ -112,7 +113,7 @@ public class SQLiteUserStorage implements UserStorageInterface {
             pstatment.close();
             
         } catch (SQLException e) {
-            throw new RuntimeException("Ошибка обновления данных пользователя", e);
+            throw new UserStorageException("Ошибка обновления данных пользователя", e);
         }
     }
 
@@ -130,7 +131,7 @@ public class SQLiteUserStorage implements UserStorageInterface {
             pstatment.close();
             
         } catch (SQLException e) {
-            throw new RuntimeException("Ошибка удаления пользователя", e);
+            throw new UserStorageException("Ошибка удаления пользователя", e);
         }
     }
 
@@ -152,7 +153,7 @@ public class SQLiteUserStorage implements UserStorageInterface {
             return exists;
             
         } catch (SQLException e) {
-            throw new RuntimeException("Ошибка проверки существования пользователя", e);
+            throw new UserStorageException("Ошибка проверки существования пользователя", e);
         }
     }
     
